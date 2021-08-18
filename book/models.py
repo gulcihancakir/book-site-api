8d0384d050cb44009f2a957117494ee2346c1eb8
GÜLCİHAN ÇAKIR <gulcihan.cakir@semtr.com>
2021-08-18 10:58:03 +0300

    ReadingListModel created

--- book/models.py
+++ book/models.py
@@ -5,8 +5,12 @@ from author.models import AuthorModel
 class TypeOfBookModel(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class BookModel(models.Model):
     name = models.CharField(max_length=100)
+    image = models.ImageField(null=True, blank=True)
     author = models.ForeignKey(AuthorModel, on_delete=models.CASCADE)
     typeofbook = models.ManyToManyField(TypeOfBookModel)
     rating = models.IntegerField(default=0, 
@@ -20,6 +24,14 @@ class BookModel(models.Model):
     publisher = models.CharField(max_length=100)
     book_summary = models.TextField()
 
+    def __str__(self):
+        return self.name
+
+class ReadingListModel(models.Model):
+    book = models.ManyToManyField(BookModel)
+
+    
+    
 
     
     
