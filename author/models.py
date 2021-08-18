28d052b8d16d3626e308052386c4a3043c775d08
GÜLCİHAN ÇAKIR <gulcihan.cakir@semtr.com>
2021-08-18 10:47:06 +0300

    __str__ function added inside model.

--- author/models.py
+++ author/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 class AuthorModel(models.Model):
+    fullname = models.CharField(max_length=100)
     unvan = models.CharField(max_length=100)
     birth = models.CharField(max_length=100)
     death = models.CharField(max_length=100)
     life = models.TextField()
+    
+
+    def __str__(self):
+        return self.fullname 
