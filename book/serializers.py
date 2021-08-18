493a68068a62d1543bdee6211fbd74d0a93d1108
GÜLCİHAN ÇAKIR <gulcihan.cakir@semtr.com>
2021-08-18 10:59:19 +0300

    serializers created

---
+++ book/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import BookModel, ReadingListModel, TypeOfBookModel
+
+
+class TypeOfBookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TypeOfBookModel
+        fields = ['name']
+
+
+class BookSerializer(serializers.ModelSerializer):
+    typeofbook = TypeOfBookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = BookModel
+        fields = ['name', 'image', 'page_number',
+                  'language', 'country', 'publisher', 'typeofbook']
+
+
+class ReadingListSerializer(serializers.ModelSerializer):
+    book = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ReadingListModel
+        fields = ['book']
