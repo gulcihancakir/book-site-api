8302a7b92f9f9acacb97d3fdd52171e84582703d
GÜLCİHAN ÇAKIR <gulcihan.cakir@semtr.com>
2021-08-18 11:12:49 +0300

    QuotationModel created

--- quotation/models.py
+++ quotation/models.py
@@ -1,3 +1,13 @@
+from account.models import UserModel
+from book.models import BookModel
 from django.db import models
 
-# Create your models here.
+
+class QuotationModel(models.Model):
+    title = models.CharField(max_length=100, null=True, blank=True)
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    quotation = models.TextField()
+    book = models.ForeignKey(BookModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
