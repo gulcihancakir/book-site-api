d426713d5a6bb6436998bbbc683fb6586659afdf
GÜLCİHAN ÇAKIR <gulcihan.cakir@semtr.com>
2021-08-19 14:46:13 +0300

    rating fields removed from BookModel.

--- book/models.py
+++ book/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
 from author.models import AuthorModel
+
 
 class TypeOfBookModel(models.Model):
     name = models.CharField(max_length=50)
@@ -8,16 +8,12 @@ class TypeOfBookModel(models.Model):
     def __str__(self):
         return self.name
 
+
 class BookModel(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(null=True, blank=True)
     author = models.ForeignKey(AuthorModel, on_delete=models.CASCADE)
     typeofbook = models.ManyToManyField(TypeOfBookModel)
-    rating = models.IntegerField(default=0, 
-            validators=[
-                MaxValueValidator(5),
-                MinValueValidator(0)
-    ])
     page_number = models.IntegerField()
     language = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
@@ -27,13 +23,6 @@ class BookModel(models.Model):
     def __str__(self):
         return self.name
 
+
 class ReadingListModel(models.Model):
     book = models.ManyToManyField(BookModel)
-
-    
-    
-
-    
-    
-
-
