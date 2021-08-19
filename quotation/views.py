a1b04fa8e87f6c9e634ec743be0220518fcb310a
GÜLCİHAN ÇAKIR <gulcihan.cakir@semtr.com>
2021-08-19 15:16:42 +0300

    QuotationPostView added.

--- quotation/views.py
+++ quotation/views.py
@@ -1,8 +1,17 @@
 from .models import QuotationModel
 from .serializers import QuotationSerializer
 from rest_framework import generics
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class QuotationView(generics.ListAPIView):
+    
+    queryset = QuotationModel.objects.all()
+    serializer_class = QuotationSerializer
 
 
-class QuotationView(generics.ListCreateAPIView):
+class QuotationPostView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated, ]
+    authentication_classes = [SessionAuthentication, ]
     queryset = QuotationModel.objects.all()
     serializer_class = QuotationSerializer
