58871a377ffb87e25f9d02799c9504402a6f8ad1
GÜLCİHAN ÇAKIR <gulcihan.cakir@semtr.com>
2021-08-24 10:33:24 +0300

    image field added.

--- author/models.py
+++ author/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class AuthorModel(models.Model):
-    fullname = models.CharField(max_length=100)
-    unvan = models.CharField(max_length=100)
-    birth = models.CharField(max_length=100)
-    death = models.CharField(max_length=100)
+    fullname = models.CharField(max_length=250)
+    image = models.ImageField(null=True, blank=True)
+    unvan = models.CharField(max_length=250)
+    birth = models.CharField(max_length=250)
+    death = models.CharField(max_length=250)
     life = models.TextField()
     
 
