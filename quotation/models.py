8c416de3d36e65fa589e3cf39148986bc71a04a8
Gülcihan Çakır <gulcihancakir8@gmail.com>
2021-08-25 14:03:34 +0300

    class Meta added.

--- quotation/models.py
+++ quotation/models.py
@@ -1,7 +1,7 @@
 from account.models import UserModel
 from book.models import BookModel
 from django.db import models
-
+from rest_framework.validators import UniqueTogetherValidator
 
 class QuotationModel(models.Model):
     user = models.ForeignKey(UserModel, on_delete=models.DO_NOTHING)
@@ -9,3 +9,9 @@ class QuotationModel(models.Model):
     quotation = models.TextField()
     quotation_page = models.IntegerField(null=True, blank=True)
     published_date = models.DateTimeField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['book'], name='same_book_quotation_constraints')
+        ]
+
