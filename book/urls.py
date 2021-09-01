de5a5bdd1204fc98d29057d156f431065514f181
Gülcihan Çakır <gulcihancakir8@gmail.com>
2021-09-01 11:54:10 +0300

    readinglist, reads   added and users can search by book name.

--- book/urls.py
+++ book/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.ReadingListView.as_view()),
-   
+    path('', views.BookListView.as_view()),
+    path('reading-post', views.ReadingPostView.as_view()),
+    path('reads-post', views.ReadsPostView.as_view()),
+    path('reading-delete/<int:pk>', views.ReadingListDeleteView.as_view()),
+    path('reads-delete/<int:pk>', views.ReadsDeleteView.as_view()),
+    path('search/<str:name>', views.BookSearchView.as_view()),    
+    
 ]
