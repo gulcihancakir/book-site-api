371c72a6ea33fad322ca01fb5f8db25b214db24c
Gülcihan Çakır <gulcihancakir8@gmail.com>
2021-09-01 12:04:30 +0300

    quotation_page, quotation fields added to constraints

--- quotation/models.py
+++ quotation/models.py
@@ -1,17 +1,17 @@
 from account.models import UserModel
 from book.models import BookModel
 from django.db import models
-from rest_framework.validators import UniqueTogetherValidator
+
 
 class QuotationModel(models.Model):
     user = models.ForeignKey(UserModel, on_delete=models.DO_NOTHING)
     book = models.ForeignKey(BookModel, on_delete=models.DO_NOTHING)
     quotation = models.TextField()
     quotation_page = models.IntegerField(null=True, blank=True)
-    published_date = models.DateTimeField()
+    published_date = models.DateTimeField(auto_now=True)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['book'], name='same_book_quotation_constraints')
+            models.UniqueConstraint(fields=[
+                                    'book', 'quotation_page', 'quotation'], name='same_book_quotation_constraints')
         ]
-
