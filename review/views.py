1269af7f8ba91df966d4aba1ca184e1bcaa10dff
Gülcihan Çakır <gulcihancakir8@gmail.com>
2021-09-01 12:30:14 +0300

    review APIView added.

--- review/views.py
+++ review/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from account.permissions import IsOwner
+from account.models import UserModel
+from rest_framework import permissions
+from .models import ReviewModel
+from .serializers import ReviewPostSerializer, ReviewSerializer
+from rest_framework import generics
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class ReviewView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    authentication_classes = [SessionAuthentication, ]
+    queryset = ReviewModel.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class ReviewDetailView(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    authentication_classes = [SessionAuthentication, ]
+    queryset = ReviewModel.objects.all()
+    serializer_class = ReviewPostSerializer
+
+
+class ReviewPostView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated, ]
+    authentication_classes = [SessionAuthentication, ]
+    queryset = ReviewModel.objects.all()
+    serializer_class = ReviewPostSerializer
+
+    def perform_create(self, serializer):
+        user = UserModel.objects.get(user__username=self.request.user)
+        serializer.save(user=user)
+
+
+class ReviewUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated, IsOwner]
+    authentication_classes = [SessionAuthentication, ]
+    queryset = ReviewModel.objects.all()
+    serializer_class = ReviewPostSerializer
