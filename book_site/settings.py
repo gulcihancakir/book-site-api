61d09505482641b9124742b1ec2966efa637cd77
Gülcihan Çakır <gulcihancakir8@gmail.com>
2021-09-13 11:49:31 +0300

    database mysql settings remove from comment line

--- book_site/settings.py
+++ book_site/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     
 ]
 SITE_ID = 1
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -102,22 +102,23 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'book-site-db',
-#         'USER': 'root',
-#         'PASSWORD': 'toor',
-#         'HOST': 'db',
-#         'PORT': 3306
-#     }
-# }
+
 DATABASES = {
     'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'db',
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'PORT': 3306,
+    },
 }
+# DATABASES = {
+#     'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
